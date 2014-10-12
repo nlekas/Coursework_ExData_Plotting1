86f81ff8c9adf962d065edd1bac79944f55bc897
nlekas <noah@noahlekas.com>
2014-10-12 14:57:33 -0400

    Updated some reference errors in plot1.R. Optimized some code when creating plot.
    Initial commit for plot2.R and plot2.png.
    
     Changes to be committed:
    	new file:   plot2.R
    	new file:   plot2.png
    
     Changes not staged for commit:
    	modified:   plot1.R

---
+++ plot2.R
@@ -0,0 +1,29 @@
+plot2 <- function(){
+    ## This function is for linux operating systems only.
+    ## This function assumes you unzipped the data into the working directory
+    ## into a folder named data.
+    
+    require(plyr)
+    library(plyr)
+    
+    ## STEP 1: Read Data
+    data <- read.table("data/household_power_consumption.txt", header = TRUE, sep = ";", na.strings ="?")
+    
+    ## STEP 2: Format Date/Time Columns
+    data <- mutate(data, Date_Time = strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"))
+    
+    ## STEP 3: Subset data for plot
+    start <- as.POSIXct("2007-02-01 00:00:00")
+    end <- as.POSIXct("2007-02-03 00:00:00")
+    data <- subset(data, Date_Time >= start, select=c(Date_Time, Global_active_power))
+    data <- subset(data, Date_Time < end, select=c(Date_Time, Global_active_power))
+    
+    ## Step 4: Build Plot
+    ### Open PNG file
+    png(filename = "plot2.png", width = 480, height = 480, units = "px", bg = "white")
+    ### Create plot
+    plot(data[,1], data[,2], ylab="Global Active Power (kilowatts)", type = "l", xlab = "")
+    ### Close PNG file
+    dev.off()
+}
+    
