7b4a6229cd407269bde3ca6139d7c2ff1b86b324
nlekas <noah@noahlekas.com>
2014-10-12 18:12:06 -0400

    Initial commit for plot4.png and plot4.R.
    Updated plot3.R to fix some reference issues.
    
     Changes to be committed:
    	modified:   plot3.R
    	new file:   plot4.R
    	new file:   plot4.png

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-plot3 <- function(){
+plot4 <- function(){
     ## This function is for linux operating systems only.
     ## This function assumes you unzipped the data into the working directory
     ## into a folder named data.
@@ -15,20 +15,33 @@ plot3 <- function(){
     ## STEP 3: Subset data for plot
     start <- as.POSIXct("2007-02-01 00:00:00")
     end <- as.POSIXct("2007-02-03 00:00:00")
-    data <- subset(data, Date_Time >= start, select=c(Date_Time, Sub_metering_1, Sub_metering_2, Sub_metering_3))
+    data <- subset(data, Date_Time >= start)
     data <- subset(data, Date_Time < end)
-    
+
     ## Step 4: Build Plot
     ### Open PNG file
-    png(filename = "plot3.png", width = 480, height = 480, units = "px", bg = "white")
-    ### Create plot
-    plot(data[,1], data[,2], ylab="Energy sub metering", type = "l", xlab = "")
+    png(filename = "plot4.png", width = 480, height = 480, units = "px", bg = "white")
+    par(mfrow=c(2,2))
+    
+    ### Create 1st plot
+    plot(data[,10], data[,3], ylab="Global Active Power (kilowatts)", type = "l", xlab = "")
+    
+    ## Create 2nd plot
+    plot(data[,10], data[,5], ylab="Voltage", type = "l", xlab = "datetime")
+    
+    ## Create 3rd plot
+    plot(data[,10], data[,7], ylab="Energy sub metering", type = "l", xlab = "")
     par(col="red")
-    lines(data[,1],data[,3])
+    lines(data[,10],data[,8])
     par(col="blue")
-    lines(data[,1],data[,4])
+    lines(data[,10],data[,9])
     par(col="black")
     legend("topright", lty=c(1,1), lwd=c(2.5, 2.5), col = c("black", "red","blue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+    
+    ## Create 4th plot
+    plot(data[,10], data[,4], ylab="Voltage", type = "l", xlab = "datetime")
+    
+    
     ### Close PNG file
     dev.off()
 }
